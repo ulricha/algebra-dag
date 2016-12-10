c3837dc069c5aa9ced0b4767ca3e21df13cdf031
Alexander Ulrich <alex@etc-network.de>
2016-12-10 15:27:50 +0100

    Add property inference traversal that might fail.

--- src/Database/Algebra/Rewrite/Properties.hs
+++ src/Database/Algebra/Rewrite/Properties.hs
@@ -2,8 +2,11 @@
 
 module Database.Algebra.Rewrite.Properties
     ( inferBottomUpG
+    , inferBottomUpE
     ) where
 
+import           Control.Monad.Except
+import qualified Data.Foldable               as F
 import qualified Data.IntMap                 as M
 import           Data.List
 import           Database.Algebra.Dag
@@ -15,6 +18,20 @@ inferBottomUpG :: Operator o
                -> AlgebraDag o
                -> NodeMap p
 inferBottomUpG inferWorker topOrderedNodes dag =
-    foldl' go M.empty $ reverse topOrderedNodes
+    foldl' go M.empty $ topOrderedNodes
   where
     go pm node = M.insert node (inferWorker (nodeMap dag) (operator node dag) node pm) pm
+
+-- | Infer properties for all nodes in a bottom-up traversal. Property inference
+-- might fail.
+inferBottomUpE :: (Operator o, MonadError e m)
+               => (NodeMap o -> o -> AlgNode -> NodeMap p -> m p)
+               -> [AlgNode]
+               -> AlgebraDag o
+               -> m (NodeMap p)
+inferBottomUpE inferWorker topOrderedNodes dag =
+    F.foldrM go M.empty topOrderedNodes
+  where
+    go n pm = do
+        p <- inferWorker (nodeMap dag) (operator n dag) n pm
+        pure $ M.insert n p pm
